a4b9581344cec58e6c7cc3bc5c66c559b21af308
Talal <alzuhait@sheridancollege.ca>
2024-11-04 23:30:27 -0500

    Add project files.

---
+++ Event Management and Ticket Booking System/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Event_Management_and_Ticket_Booking_System.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
